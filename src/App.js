ad99c98c5c8e1887bcf2325964527e0e50368a9d
Joan Renfroe <97361642+Jeal3R5@users.noreply.github.com>
2022-05-08 18:14:56 -0700

    functionality added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 // IMPORT useState
 import React, { useState } from "react";
 //console.log("this is React", React)
+import "../public/styles.css";
 import "./styles.css";
+
 // IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
 import imagesArr from "./imageData";
 
 export default function App() {
   // USE useState TO CREATE  [bigImage, setBigImage]
-  const [bigImage, setBigImage] = useState(imagesArr[0].img);
-
-  //console.log(bigImage)
-  //console.log(bigImage);
   // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
-
-  let staticCityImgs = useState[0];
+  const [bigImage, setBigImage] = useState(imagesArr[0].img);
+  //console.log(bigImage);
 
   // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  //const handleImage
   // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  const handleBigImage = (urlImage) => {
+    setBigImage(urlImage);
+  };
 
   // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
   // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
 
-  let images = images.return(
+  const images = imagesArr.map((element, index) => {
+    return (
+      <img
+        onClick={() => {
+          handleBigImage(element.img);
+        }}
+        src={element.img}
+        alt={element.city}
+        className={bigImage === element.img ? "border" : "thumb"}
+        key={index}
+      />
+    );
+  });
+  console.log(bigImage);
+  return (
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
-        <div id="thumbnails">{/* RENDER THE IMAGES ARRAY  */}</div>
+        <div id="thumbnails">{images}</div>
         {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        {/* <img src="" id="bigimage" alt='bigImage'/> */}
+        {<img src={bigImage} id="bigimage" alt="bigImage" />}
       </div>
     </div>
   );
